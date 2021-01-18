3c704124be467200d91ca9507c411217a6500ca6
magnuvau <austrheim.magnus@protonmail.com>
2021-01-18 20:12:08 +0100

    Renamed to init.el and set install requirements

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+;; Requirements: 'M-x package-install solarized-theme'
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
