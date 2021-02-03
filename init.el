ab0c224f5ee7d8c4a7dc469acba63bb742a36c80
magnuvau <austrheim.magnus@protonmail.com>
2021-02-03 19:02:52 +0100

    Add function to nuke buffers

--- init.el
+++ init.el
@@ -32,3 +32,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(defun nuke-all-buffers ()
+  (interactive)
+  (mapcar 'kill-buffer (buffer-list))
+  (delete-other-windows))
+
+(global-set-key (kbd "C-x K") 'nuke-all-buffers)
