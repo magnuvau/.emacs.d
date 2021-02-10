0c93962e94f43f51f0c87840fa881151fffd3a80
magnuvau <austrheim.magnus@protonmail.com>
2021-02-10 21:49:52 +0100

    Add dockerfile-mode

--- init.el
+++ init.el
@@ -1,15 +1,15 @@
 ;; Requirements:
 ;; 'M-x package-refresh-contents'
 ;; 'M-x package-install solarized-theme'
+;; 'M-x package-install dockerfile-mode'
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
-
 (load-theme 'solarized-dark t)
+
+(require 'dockerfile-mode)
+(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
 
 (global-display-line-numbers-mode)
 
@@ -19,7 +19,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(menu-bar-mode nil)
- '(package-selected-packages '(csharp-mode solarized-theme))
+ '(package-selected-packages '(dockerfile-mode csharp-mode solarized-theme))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil)
  '(tooltip-mode nil))
